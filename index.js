fff082190a12a5535d6cc29160ffd019c24b724a
zakgc <zakgriffithcrowe@gmail.com>
2025-12-02 18:50:44 +0000

    Add part 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-let rangesTxt = fs.readFileSync('./testranges.txt', 'utf-8')
+let rangesTxt = fs.readFileSync('./ranges.txt', 'utf-8')
 let rangesArr = rangesTxt.split(',')
 rangesArr.forEach((range, index) => {
     let stringArr = range.split('-')
@@ -26,5 +26,38 @@ rangesArr.forEach(rangeArr => {
     }
 })
 
+let numberOfInvalidIDs2 = 0
+rangesArr.forEach(rangeArr => {
+    for (let index = rangeArr[0]; index <= rangeArr[1]; index++) {
+        let indexString = index.toString()
+        
+        for (let i = 1; i <= indexString.length; i++) {
+            if (indexString.length % i !== 0) {
+                continue
+            }
+            
+            let size = indexString.length / i
+            let result = []
+
+            for (let x = 0; x < indexString.length; x += size) {
+                result.push(indexString.slice(x, x + size))
+            }
+
+            if (result.length === 1) {
+                continue
+            }
+
+            let matching = result.every((val, i, arr) => val === arr[0])
+
+            if (matching) {
+                numberOfInvalidIDs2 += index
+                break
+            }
+            
+        }
+    }
+})
+
 console.log(numberOfInvalidIDs);
+console.log(numberOfInvalidIDs2);
 
